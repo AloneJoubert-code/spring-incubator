fccf023faf31d468047a1742b0c4c45cf2c0b4a5
AloneJoubert-code <alone.joubert@entelect.co.za>
2026-02-17 09:19:41 +0200

    Swap RabbitMQ for ActiveMQ in notification-service
    Replace spring-boot-starter-amqp with spring-jms and
    activemq-client 6.2.0 for ActiveMQ Classic integration.
    Add JMS configuration with connection factory, Jackson message
    converter and listener container factory. Configure broker URL
    and queue name in application properties.

---
+++ notification-service/src/main/java/entelect/training/incubator/spring/notification/config/JmsConfig.java
@@ -0,0 +1,42 @@
+package entelect.training.incubator.spring.notification.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.jms.ConnectionFactory;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+@EnableJms
+public class JmsConfig {
+
+    @Bean
+    public ConnectionFactory connectionFactory(
+            @Value("${spring.activemq.broker-url}") String brokerUrl) {
+        return new ActiveMQConnectionFactory(brokerUrl);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(new ObjectMapper());
+        return converter;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(
+            ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        return factory;
+    }
+}
